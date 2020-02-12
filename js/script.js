e42f20c1f5003f9b0c5903547cb70012b1632066
Stephany Casillas <s_stephany.casillas@ousd.org>
2020-02-12 02:12:00 +0000

    saving

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-var songNames = ["Lo que siento", "The Less I know the Better", "Sugar"];
-var songArtists = ["Cuco", "Tame Impala", "Brockhampton"];
-var songLength = [""];
-var songImg = [];
-var songLink = [];
+// $('.songList').append("hello");
+// var songNames = ["Lo que ", "The Less I know the Better", "SUGAR"];
+// var songArtists = ["Cuco", "Tame Impala", "Brockhampton"];
+// var songLength = [];
+// var songImg = [];
+// var songLink = [];
 
-songNames.forEach(function(song){
-    $('.songNames').append(song + " ");
-});
+// songNames.forEach(function(song){
+//     $('.songList').append("<li>" + song + "</li>");
+// });
 
-songArtists.forEach(function(Artist){
-    $('.songArtists').append(Artist);
-});
+// songArtists.forEach(function(Artist){
+//     $('.songArtists').append("<li>" + Artist + "</li>");
+// });
 
-songLength.forEach(function(Length){
-    $('.songLength').append(Length);
-});
+// songLength.forEach(function(Length){
+//     $('.songLength').append(Length);
+// });
 
-songImg.forEach(function(){
-    $('.songImg').append();
-})
+// songImg.forEach(function(){
+//     $('.songImg').append();
+// });
