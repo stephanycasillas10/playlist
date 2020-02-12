bb3ddbd38f00cee5fc151ac68ae2524ce055f8d0
Stephany Casillas <s_stephany.casillas@ousd.org>
2020-02-12 06:55:23 +0000

    added song images and styling

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 var songNames = ["Lo que siento", "The Less I know the Better", "SUGAR"];
 var songArtists = ["Cuco", "Tame Impala", "Brockhampton"];
 var songLength = [];
-var songImg = [];
+var songImg = ["https://cont-5.p-cdn.us/images/public/int/5/9/2/9/191061299295_1080W_1080H.jpg", "https://c.saavncdn.com/807/Currents-English-2015-500x500.jpg", "https://www.guitartabsexplorer.com/images/tabs/brockhampton_sugar.jpg"];
 var songLink = [];
 
 songNames.forEach(function(song){
-    $('.songList').append("<p>" + song + "</p>");
+    $('.songList').append("<h4>" + song + "</h4>");
+    $('.songList').css("font-size", "20px");
+    $('.songList').css("font-family", "Quicksand");
 });
 
 songArtists.forEach(function(Artist){
     $('.artistList').append("<p>" + Artist + "</p>");
+    $('.artistList').css("font-size", "20px");
+    $('.artistList').css("font-family", "Quicksand");
 });
 
 songLength.forEach(function(Length){
@@ -17,5 +21,7 @@ songLength.forEach(function(Length){
 });
 
 songImg.forEach(function(imgURL){
-    $('.songImg').append("<img>" +image + "</img>");
+    $('.songImg').append("<img src=" + imgURL + ">");
+    $('img').css("width", "70px", "height", "70px")
+    
 });
